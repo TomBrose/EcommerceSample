c930bbad6bfd8eb1892fdbfe88fcf351860ca9c6
Thomas Ambrose <thomasm.ambrose@gmail.com>
2020-10-13 14:15:18 -0400

    Cookie handling added

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({
+    keys:['dwadwd6dwda7']
+}));
 
 // route handler
-app.get('/', (reg, res) => {
+app.get('/', (req, res) => {
     res.send(`
     <div>
+       Your ID is: ${req.session.userId}
         <form method="POST">
             <input name="email" placeholder="Email" />
             <input name="password" placeholder="Password" />
@@ -21,8 +27,22 @@ app.get('/', (reg, res) => {
 
 
 // Post routes
-app.post('/', (req, res) => {
-    console.log(req.body);
+app.post('/', async (req, res) => {
+    const { email, password, passwordConfirmation } = req.body;
+
+    const existingUser = await usersRepo.getOneBy({ email: email });
+    if(existingUser) {
+        return res.send('Email already in use!');
+    }
+
+    if (password !== passwordConfirmation) {
+        return res.send('Passwords must match!');
+    }
+
+    const user = await usersRepo.create({ email, password });
+
+    req.session.userId = user.id;
+
     res.send("account created");
 });
 
