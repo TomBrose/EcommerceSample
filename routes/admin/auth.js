fa0b23634019798b1b2e74079cdc59b20ae23f71
Thomas Ambrose <thomasm.ambrose@gmail.com>
2020-10-15 22:52:26 -0400

    all edit/delete routes & forms added

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -20,20 +20,14 @@ router.get('/signup', (req, res) => {
 
 router.post(
   '/signup',
-  [requireEmail, requirePassword, requirePasswordConfirmation],
+  [requireEmail, requirePassword, requirePasswordConfirmation], handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
     const user = await usersRepo.create({ email, password });
 
     req.session.userId = user.id;
 
-    res.send('Account created!!!');
+    res.redirect('/admin/products');
   }
 );
 
@@ -48,21 +42,14 @@ router.get('/signin', (req, res) => {
 
 router.post(
   '/signin',
-  [requireEmailExists, requireValidPasswordForUser],
+  [requireEmailExists, requireValidPasswordForUser], handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    
-    if (!errors.isEmpty()) {
-        return res.send(signinTemplate({ errors }));
-    }
-
     const { email } = req.body;
-
     const user = await usersRepo.getOneBy({ email });
 
     req.session.userId = user.id;
 
-    res.send('You are signed in!!!');
+    res.redirect('/admin/products');
   }
 );
 
